42a5c198dde59c1ede02d3817623dcacf8646782
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-09-16 13:03:13 -0400

    blink led on sensor value

--- GobotGPG3Outline.go
+++ GobotGPG3Outline.go
@@ -8,28 +8,36 @@ import (
 	"gobot.io/x/gobot/platforms/raspi"
 	"time"
 )
+
 //robotRunLoop is the main function for the robot, the gobot framework
 //will spawn a new thread in the NewRobot factory functin and run this
 //function in that new thread. Do all of your work in this function and
 //in other functions that this function calls. don't read from sensors or
 //use actuators frmo main or you will get a panic.
-func robotRunLoop(lightSensor *aio.GroveLightSensorDriver){
+func robotRunLoop(gopigo3 *g.Driver, lightSensor *aio.GroveLightSensorDriver) {
 	for {
 		sensorVal, err := lightSensor.Read()
-		if err!=nil{
+		if err != nil {
 			fmt.Errorf("Error reading sensor %+v", err)
 		}
 		fmt.Println("Light Value is ", sensorVal)
+
+		if sensorVal > 100 {
+			_ = gopigo3.SetLED(g.LED_EYE_RIGHT, 0x00, 0x00, uint8(0xFF))
+		} else {
+			_ = gopigo3.SetLED(g.LED_EYE_RIGHT, 0x00, 0x00, uint8(0x00))
+		}
+
 		time.Sleep(time.Second)
 	}
 }
 
-func main(){
+func main() {
 	//We create the adaptors to connect the GoPiGo3 board with the Raspberry Pi 3
 	//also create any sensor drivers here
 	raspiAdaptor := raspi.NewAdaptor()
 	gopigo3 := g.NewDriver(raspiAdaptor)
-	lightSensor:= aio.NewGroveLightSensorDriver(gopigo3, "AD_2_1") //AnalogDigital Port 1 is "AD_1_1" this is port 2
+	lightSensor := aio.NewGroveLightSensorDriver(gopigo3, "AD_2_1") //AnalogDigital Port 1 is "AD_1_1" this is port 2
 	//end create hardware drivers
 
 	//here we create an anonymous function assigned to a local variable
@@ -37,16 +45,15 @@ func main(){
 	//I'm calling my robot main loop here. Pass any of the variables we created
 	//above to that function if you need them
 	mainRobotFunc := func() {
-		robotRunLoop(lightSensor)
+		robotRunLoop(gopigo3, lightSensor)
 	}
-
 
 	//this is the crux of the gobot framework. The factory function to create a new robot
 	//struct (go uses structs and not objects) It takes four parameters
 	robot := gobot.NewRobot("gopigo3sensorChecker", //first a name
-		[]gobot.Connection{raspiAdaptor}, //next a slice of connections to one or more robot controllers
+		[]gobot.Connection{raspiAdaptor},     //next a slice of connections to one or more robot controllers
 		[]gobot.Device{gopigo3, lightSensor}, //next a slice of one or more sensors and actuators for the robots
-		mainRobotFunc, //the variable holding the function to run in a new thread as the main function
+		mainRobotFunc,                        //the variable holding the function to run in a new thread as the main function
 	)
 
 	robot.Start() //actually run the function
